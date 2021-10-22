9dd68f3459c8aa4a43d230c34df93aaf751b4d29
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2021-10-22 15:35:54 +0200

    chore: renema

--- pages/api/test.js
+++ pages/api/test.js
@@ -5,11 +5,11 @@ import resolveCommit from "../../src/resolveCommit";
 export default async function hello(req, res) {
   const fromReleases = await getCommitsBetweenReleases();
 
-  const allll = await Promise.all(
+  const nestedCommits = await Promise.all(
     fromReleases.map((commit) => resolveCommit(commit.sha))
   );
 
-  const allCommits = allll.flat(1);
+  const allCommits = nestedCommits.flat(1);
 
   res.status(200).json({ fromReleases, allll, allCommits });
 }
