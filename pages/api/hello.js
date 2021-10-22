40423690c3323890dc5cd96cdb9e8fcf81a5da84
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2021-10-22 13:14:11 +0200

    commit 3

--- pages/api/hello.js
+++
@@ -1,7 +0,0 @@
-import getCommitsBetweenReleases from "../../src/getCommitsBetweenReleases";
-
-export default async function hello(req, res) {
-  const cleanCommits = await getCommitsBetweenReleases();
-
-  res.status(200).json(cleanCommits);
-}
