ce67b6438391ef57e88eeed030852d704603921c
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2021-10-22 15:40:37 +0200

    Test branch 00003 (#12)

--- pages/api/test.js
+++ pages/api/test.js
@@ -5,9 +5,11 @@ import resolveCommit from "../../src/resolveCommit";
 export default async function hello(req, res) {
   const fromReleases = await getCommitsBetweenReleases();
 
-  const allll = await Promise.all(
+  const nestedCommits = await Promise.all(
     fromReleases.map((commit) => resolveCommit(commit.sha))
   );
 
-  res.status(200).json({ fromReleases, allll });
+  const allCommits = nestedCommits.flat(1);
+
+  res.status(200).json({ fromReleases, allll, allCommits });
 }
